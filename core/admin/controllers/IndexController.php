b2585feb15eef43427b2f891e584103b749befc7
veden <stud0000248921@study.utmn.ru>
2022-12-08 05:03:59 +0500

    get запрос методы fields where order

--- core/admin/controllers/IndexController.php
+++ core/admin/controllers/IndexController.php
@@ -12,6 +12,19 @@ class IndexController extends BaseController
 
         $db = Model::instance();
 
+        $table = 'articles';
+
+        $color = ['red','blue','black'];
+
+        $res = $db->get($table,[
+            'fields' =>['id','name',],
+            'where' =>['name'=>'Roma,ivan' ,'id'=>'Roma', 'price'=>'457','car'=>'porshe', 'color'=> $color],
+            'operand'=>['IN','%LIKE%','<>','=','NOT IN'],
+            'condition'=>['AND','OR','AND'],
+            'order'=>['name','price'],
+            'order_direction'=>['DESC'],
+            'Limit'=>'1'
+        ]);
 
         exit();
 
--- core/base/models/BaseModel.php
+++ core/base/models/BaseModel.php
@@ -70,5 +70,190 @@ class BaseModel
 
     }
 
+    /**
+     * @param $table - Таблица бд
+     * @param array $set
+     * 'fields' = ['id','name']
+     * 'where' =>['id'=>1,'name'=>'Roma'],
+     * 'operand'=>['<>','='],
+     * 'condition'=>['AND'],
+     * 'order'=>['name'],
+     * 'order_direction'=>['ASC','DESC'],
+     * 'Limit'=>'1'
+     */
+
+    final public function get($table ,$set=[]){
+
+        $fields = $this->createFields($table,$set);
+
+        $where = $this->createWhere($table,$set);
+
+        if(!$where) $new_where = true;
+            else $new_where = false;
+
+        $join_arr = $this->createJoin($table,$set,$new_where);
+
+        $fields .=$join_arr['fields'];
+        $join = $join_arr['join'];
+        $where .=$join_arr['where'];
+
+        $fields = rtrim($fields,',');
+
+        $order = $this->createOrder($table,$set);
+
+        $limit = $set['limit']?$set['limit']:'';
+
+        $query = "SELECT $fields FROM $table $join $where $order $limit";
+
+        return $this->query($query);
+
+    }
+
+//    создание полей
+
+    protected function createFields($table = false,$set){
+
+        $set['fields'] = is_array($set['fields']) && !empty($set['fields'])? $set['fields']:['*'];
+
+        $table = $table ? $table . '.' :'';
+
+        $fields = '';
+
+        foreach ($set['fields'] as $field){
+            $fields .= $table . $field. ',';
+        }
+
+        return $fields;
+    }
+
+//    создание сортировки
+
+    protected function createOrder($table = false,$set){
+
+        $table = $table ? $table . '.' :'';
+
+        $order_by = '';
+
+        if(is_array($set['order']) && !empty($set['order'])){
+
+            $set['order_direction'] = is_array($set['order_direction']) && !empty($set['order_direction'])? $set['order_direction']:['ASC'];
+
+            $order_by ='ORDER BY ';
+            $direct_count = 0;
+
+            foreach ($set['order'] as $order){
+                if($set['order_direction'][$direct_count]){
+                    $order_direction = strtoupper($set['order_direction'][$direct_count]);
+                    $direct_count++;
+                }else{
+                    $order_direction = strtoupper($set['order_direction'][$direct_count-1]);
+                }
+
+                if(is_int($order)) $order_by .= $order . ' ' . $order_direction . ',';
+                    else $order_by .= $table . $order . ' ' . $order_direction . ',';
+
+            }
+
+            $order_by = rtrim($order_by, ',');
+        }
+
+        return $order_by;
+    }
+
+//    создание условия
+
+    protected function createWhere($table = false,$set, $instruction = 'WHERE'){
+
+        $table = $table ? $table . '.' :'';
+
+        $where = '';
+
+        if(is_array($set['where']) && !empty($set['where'])){
+
+            $set['operand'] = is_array($set['operand']) && !empty($set['operand'])?$set['operand']:['='];
+            $set['condition'] = is_array($set['condition']) && !empty($set['condition'])?$set['condition']:['AND'];
+
+            $where = $instruction;
+
+            $o_count = 0;
+            $c_count = 0;
+
+            foreach ($set['where'] as $key=>$item){
+
+                $where .= ' ';
+
+                if($set['operand'][$o_count]){
+                    $operand = $set['operand'][$o_count];
+                    $o_count++;
+                }else{
+                    $operand = $set['operand'][$o_count-1];
+                }
+
+                if($set['condition'][$c_count]){
+                    $condition = $set['condition'][$c_count];
+                    $c_count++;
+                }else{
+                    $condition = $set['condition'][$c_count-1];
+                }
+
+                if($operand ==='IN' || $operand === 'NOT IN'){
+                    if(is_string($item) && strpos($item , 'SELECT')){
+                        $in_str = $item;
+                    }else{
+                        if(is_array($item)) $temp_item = $item;
+                            else $temp_item = explode(',',$item);
+
+                        $in_str = '';
+
+                        foreach ($temp_item as $v){
+                            $in_str.= "'" . trim($v) . "',";
+                        }
+
+                    }
+                    $where .= $table . $key .' ' . $operand . ' (' . rtrim($in_str,',') . ') ' . $condition;
+
+                }elseif(strpos($operand, 'LIKE') !== false ){
+
+                    $like_template = explode('%', $operand);
+
+                    foreach ($like_template as $lt_key=> $lt){
+                        if(!$lt){
+                            if(!$lt_key){
+                                $item = '%' . $item;
+                            }else{
+                                $item .= '%';
+                            }
+                        }
+
+
+                    }
+
+                    $where .= $table . $key . ' LIKE ' . "'" . $item . "' $condition";
+
+                }else{
+
+                    if(strpos($item , 'SELECT')===0){
+                        $where .= $table . $key . ' ' . $operand . ' (' . $item . ') ' . $condition;
+                    }else{
+                        $where .= $table . $key . ' ' . $operand . " '" . $item . "' " . $condition;
+                    }
+
+                }
+
+
+
+            }
+
+            $where = substr($where , 0, strrpos($where, $condition));
+
+        }
+
+        return $where;
+
+    }
+
+    protected function createJoin($table = false,$set,$where){
+
+    }
 
 }
